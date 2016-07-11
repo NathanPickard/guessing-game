569343447a24c3d4b4c880edef3c2c6df17ab24f
Nathan Pickard <nathan.pickard87@gmail.com>
2016-07-11 15:11:50 -0700

    Added 'getUserInput' 'getUserName' and 'guessNumber' functions

--- game.js
+++ game.js
@@ -1,3 +1,85 @@
+
+var userName = getUserName();
+
+alert("Hello " + userName + "!  Welcome to my website.  Enjoy!");
+
+var guess1 = prompt("Did I live in Tennessee for 15 years?", "");
+//while (guess1 == "Y" || guess1 == "y" || guess1 == "YES" || guess1 == "N" || guess1 == "n" || guess1 == "NO") {
+//  alert("Please enter 'yes' or 'no'");
+//  var guess1 = prompt("Did I live in Tennessee for 15 years?", "");
+//}
+getUserInput(guess1);
+//if (guess1.toLowerCase() == "yes") {
+//  alert("That is correct! Way to go!");
+//  var correctAnswers = 1;
+//}
+//else {
+// alert("Incorrect, I actually lived south of Nashville for 15 years!");
+//  var correctAnswers = 0;
+//}
+
+
+var showAnswer1 = document.getElementById("answer1");
+if (guess1.toLowerCase() == "yes") {
+  showAnswer1.innerHTML =  "Question 1: " + guess1 + " is correct!";
+  var correctAnswers = 1;
+} else {
+  showAnswer1.innerHTML = "Incorrect, I actually lived south of Nashville for 15 years!";
+  var correctAnswers = 0;
+}
+
+
+
+var guess2 = prompt("Are anchovies my favorite pizza topping?");
+while (guess2 == "Y" || guess2 == "y" || guess2 == "YES" || guess2 == "N" || guess2 == "n" || guess2 == "NO") {
+
+  var guess2 = prompt("Are anchovies my favorite pizza topping?");
+}
+if (guess2.toLowerCase() == "no") {
+  alert("You got it right!  My favorite pizza topping is bacon!");
+  correctAnswers++;
+} else {
+  alert("Nope! Not quite, I do enjoy anchovies on my pizza though!")
+}
+console.log(userName + " guessed: " + guess2 + " for question 2");
+
+
+
+var guess3 = prompt("Have I ever hiked to the summit of Middle Sister?");
+while (guess3 == "Y" || guess3 == "y" || guess3 == "YES" || guess3 == "N" || guess3 == "n" || guess3 == "NO") {
+  alert("Please enter 'yes' or 'no'");
+  var guess3 = prompt("Have I ever hiked to the summit of Middle Sister?");
+}
+if (guess3.toLowerCase() == "yes") {
+  alert("You are correct! I started the hike in mid-afternoon and hiked back down in the evening!");
+  correctAnswers++;
+} else {
+  alert("Actually, I have, " + userName + "!  I really recommend hiking up to the summit yourself!");
+}
+
+guessNumber();
+
+alert("You answered " + correctAnswers + " questions out of 4 correct, " + userName + "!");
+
+function getUserName() {
+  var userName = "";
+  while((userName.trim() == "") && (userName != null)) {
+    var userName = prompt("Please tell me your name:", "Enter here");
+  }
+  if (userName != null) {
+    return userName;
+  } else {
+    return "";
+  }
+}
+
+function getUserInput(guess) {
+  while (guess != "yes" && guess != "no") {
+    alert("Please enter 'yes' or 'no'");
+  }
+  return guess;
+}
+
 function guessNumber(guess) {
   var guess4 = prompt("Guess my favorite number from 1 to 10!");
   while (guess4 != 8) {
@@ -14,65 +96,3 @@ function guessNumber(guess) {
     correctAnswers++;
   }
 }
-
-function getUserName() {
-  var userName = "";
-  while((userName.trim() == "") && (userName != null)) {
-    var userName = prompt("Please tell me your name:", "Enter here");
-  }
-}
-
-
-getUserName();
-alert("Hello " + userName + "!  Welcome to my website.  Enjoy!");
-console.log("User Name: " + userName);
-console.log("Get a guess from the user.");
-
-var guess1 = prompt("Did I live in Tennessee for 15 years?", "");
-while (guess1 == "Y" || guess1 == "y" || guess1 == "YES" || guess1 == "N" || guess1 == "n" || guess1 == "NO") {
-  alert("Please enter 'yes' or 'no'");
-  var guess1 = prompt("Did I live in Tennessee for 15 years?", "");
-}
-if (guess1.toLowerCase() == "yes") {
-  alert("That is correct! Way to go!");
-  var correctAnswers = 1;
-}
- else {
-  alert("Incorrect, I actually lived south of Nashville for 15 years!");
-  var correctAnswers = 0;
-}
-console.log(userName + " guessed: " + guess1 + " for question 1");
-
-
-
-var guess2 = prompt("Are anchovies my favorite pizza topping?");
-while (guess2 == "Y" || guess2 == "y" || guess2 == "YES" || guess2 == "N" || guess2 == "n" || guess2 == "NO") {
-  alert("Please enter 'yes' or 'no'");
-  var guess2 = prompt("Are anchovies my favorite pizza topping?");
-}
-if (guess2.toLowerCase() == "no") {
-  alert("You got it right!  My favorite pizza topping is bacon!");
-  correctAnswers++;
-} else {
-  alert("Nope! Not quite, I do enjoy anchovies on my pizza though!")
-}
-console.log(userName + " guessed: " + guess2 + " for question 2");
-
-var guess3 = prompt("Have I ever hiked to the summit of Middle Sister?");
-while (guess3 == "Y" || guess3 == "y" || guess3 == "YES" || guess3 == "N" || guess3 == "n" || guess3 == "NO") {
-  alert("Please enter 'yes' or 'no'");
-  var guess3 = prompt("Have I ever hiked to the summit of Middle Sister?");
-}
-if (guess3.toLowerCase() == "yes") {
-  alert("You are correct! I started the hike in mid-afternoon and hiked back down in the evening!");
-  correctAnswers++;
-} else {
-  alert("Actually, I have, " + userName + "!  I really recommend hiking up to the summit yourself!");
-}
-console.log(userName + " guessed: " + guess3 + " for question 3");
-
-
-
-
-
-alert("You answered " + correctAnswers + " questions out of 4 correct, " + userName + "!");
