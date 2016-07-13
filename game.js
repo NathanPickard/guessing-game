67f5e147b6fc3402b3253679a4b6a3ed9e6b66fb
Nathan Pickard <nathan.pickard87@gmail.com>
2016-07-13 15:26:22 -0700

    Added the first image and removed old code

--- game.js
+++ game.js
@@ -4,19 +4,7 @@ var userName = getUserName();
 alert("Hello " + userName + "!  Welcome to my website.  Enjoy!");
 
 var guess1 = prompt("Did I live in Tennessee for 15 years?", "");
-//while (guess1 == "Y" || guess1 == "y" || guess1 == "YES" || guess1 == "N" || guess1 == "n" || guess1 == "NO") {
-//  alert("Please enter 'yes' or 'no'");
-//  var guess1 = prompt("Did I live in Tennessee for 15 years?", "");
-//}
 getUserInput(guess1);
-//if (guess1.toLowerCase() == "yes") {
-//  alert("That is correct! Way to go!");
-//  var correctAnswers = 1;
-//}
-//else {
-// alert("Incorrect, I actually lived south of Nashville for 15 years!");
-//  var correctAnswers = 0;
-//}
 
 var showAnswer1 = document.getElementById("answer1");
 if (guess1.toLowerCase() == "yes") {
@@ -27,7 +15,6 @@ if (guess1.toLowerCase() == "yes") {
   var correctAnswers = 0;
 }
 
-
 var guess2 = prompt("Are anchovies my favorite pizza topping?");
 getUserInput(guess2);
 var showAnswer2 = document.getElementById("answer2");
@@ -37,43 +24,23 @@ if (guess2.toLowerCase() == "yes") {
   showAnswer2.innerHTML = guess2 + " is the correct answer!  My favorite pizza topping is bacon!";
   correctAnswers++;
 }
-//while (guess2 == "Y" || guess2 == "y" || guess2 == "YES" || guess2 == "N" || guess2 == "n" || guess2 == "NO") {
-//
-//  var guess2 = prompt("Are anchovies my favorite pizza topping?");
-//}
-//if (guess2.toLowerCase() == "no") {
-//  alert("You got it right!  My favorite pizza topping is bacon!");
-//  correctAnswers++;
-//} else {
-//  alert("Nope! Not quite, I do enjoy anchovies on my pizza though!")
-//}
-//console.log(userName + " guessed: " + guess2 + " for question 2");
-
-
 
 var guess3 = prompt("Have I ever hiked to the summit of Middle Sister?");
 getUserInput(guess3);
 var showAnswer3 = document.getElementById("answer3");
+var showPicture3 = document.getElementById("image3");
 if (guess3.toLowerCase() == "yes") {
   showAnswer3.innerHTML = guess3 + " is the right answer!  I started the hike in mid-afternoon and hiked back down in the evening!";
+  showPicture3.innerHTML = '<img src="middle-sister-07.23.11.jpg"/>';
   correctAnswers++;
 } else {
   showAnswer3.innerHTML = "No is incorrect, " + userName + "!  I really recommend hiking up to the summit yourself!";
 }
-//while (guess3 == "Y" || guess3 == "y" || guess3 == "YES" || guess3 == "N" || guess3 == "n" || guess3 == "NO") {
-//  alert("Please enter 'yes' or 'no'");
-//  var guess3 = prompt("Have I ever hiked to the summit of Middle Sister?");
-//}
-//if (guess3.toLowerCase() == "yes") {
-//  alert("You are correct! I started the hike in mid-afternoon and hiked back down in the evening!");
-//  correctAnswers++;
-//} else {
-//  alert("Actually, I have, " + userName + "!  I really recommend hiking up to the summit yourself!");
-//}
 
 guessNumber();
 
-alert("You answered " + correctAnswers + " questions out of 4 correct, " + userName + "!");
+var numberOfCorrectAnswers = document.getElementById("correctAnswers")
+numberOfCorrectAnswers.innerHTML = "You answered " + correctAnswers + " questions out of 4 correct, " + userName + "!";
 
 function getUserName() {
   var userName = "";
