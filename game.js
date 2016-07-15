06ce6353a8ef978a5650eb9ed64efdf015d26786
Nathan Pickard <nathan.pickard87@gmail.com>
2016-07-14 21:28:42 -0700

    Added colors and styling

--- game.js
+++ game.js
@@ -8,11 +8,11 @@ getUserInput(guess1);
 var showAnswer1 = document.getElementById("answer1");
 var showPicture1 = document.getElementById("image1");
 if (guess1.toLowerCase() == "yes") {
-  showAnswer1.innerHTML =  guess1 + " is correct!";
+  showAnswer1.innerHTML = "<span class='correctAnswerColor'>" + guess1 + " is correct!" + "</span>";// guess1 + " is correct!";
   showPicture1.innerHTML = '<img src= "nashville-skyline.jpg"/>';
   var correctAnswers = 1;
 } else {
-  showAnswer1.innerHTML = "Incorrect, I actually lived south of Nashville for 15 years!";
+  showAnswer1.innerHTML = "<span class='incorrectAnswerColor'>" + "Incorrect, I actually lived south of Nashville for 15 years!" + "</span>";
   var correctAnswers = 0;
 }
 
@@ -21,9 +21,9 @@ getUserInput(guess2);
 var showAnswer2 = document.getElementById("answer2");
 var showPicture2 = document.getElementById("image2");
 if (guess2.toLowerCase() == "yes") {
-  showAnswer2.innerHTML = "Nope! Not quite, I do enjoy anchovies on my pizza though!";
+  showAnswer2.innerHTML = "<span class='incorrectAnswerColor'>" + "Nope! Not quite, I do enjoy anchovies on my pizza though!" + "</span>";
 } else {
-  showAnswer2.innerHTML = guess2 + " is the correct answer!  My favorite pizza topping is bacon!";
+  showAnswer2.innerHTML = "<span class='correctAnswerColor'>" + guess2 + " is the correct answer!  My favorite pizza topping is bacon!" + "</span>";
   showPicture2.innerHTML = '<img src="CIMG2912.jpg"/>';
   correctAnswers++;
 }
@@ -33,11 +33,11 @@ getUserInput(guess3);
 var showAnswer3 = document.getElementById("answer3");
 var showPicture3 = document.getElementById("image3");
 if (guess3.toLowerCase() == "yes") {
-  showAnswer3.innerHTML = guess3 + " is the right answer!  I started the hike in mid-afternoon and hiked back down in the evening!";
+  showAnswer3.innerHTML = "<span class='correctAnswerColor'>" + guess3 + " is the right answer!  I started the hike in mid-afternoon and hiked back down in the evening!" + "</span>";
   showPicture3.innerHTML = '<img src="middle-sister-07.23.11.jpg"/>';
   correctAnswers++;
 } else {
-  showAnswer3.innerHTML = "No is incorrect, " + userName + "!  I really recommend hiking up to the summit yourself!";
+  showAnswer3.innerHTML = "<span class='incorrectAnswerColor'>" + "No is incorrect, " + userName + "!  I really recommend hiking up to the summit yourself!" + "</span>";
 }
 
 guessNumber();
@@ -58,16 +58,20 @@ function getUserName() {
 }
 
 function getUserInput(guess) {
-  while (guess != "yes" && guess != "no") {
+  //if (guess != "yes" )
+  //do {
+
+   while (guess != "yes" && guess != "no") {
     alert("Please enter 'yes' or 'no'");
+//  prompt()
   }
   return guess;
 }
 
 function guessNumber(guess) {
   var guess4 = prompt("Guess my favorite number from 1 to 10!");
-  while (guess4 != 8) {
-    if (guess4 < 8) {
+  while (guess4 != 9) {
+    if (guess4 < 9) {
       alert("Your guess is too low, guess again!");
 
     } else {
@@ -75,9 +79,9 @@ function guessNumber(guess) {
     }
     var guess4 = prompt("Guess my favorite number from 1 to 10!");
   }
-  if (guess4 == 8){
+  if (guess4 == 9){
     var showAnswer4 = document.getElementById("answer4");
-    showAnswer4.innerHTML = guess4 + " is correct, way to go!"
+    showAnswer4.innerHTML = "<span class='correctAnswerColor'>" + guess4 + " is correct, way to go!"
     correctAnswers++;
   }
 }
